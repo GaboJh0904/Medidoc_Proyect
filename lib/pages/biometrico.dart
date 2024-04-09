8e919840a1c3330d4af142b90d3f946e886c8ee6
Ismael Rivera <ismael.rivera.work@gmail.com>
2024-04-09 15:36:52 -0400

    Autenticacion Biometrica

---
+++ lib/pages/biometrico.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:medidoc_proyect/logica_Autorizacion.dart';
+
+class BioPag extends StatefulWidget {
+  const BioPag({super.key});
+
+  @override
+  State<BioPag> createState() => _BioPagState();
+}
+
+class _BioPagState extends State<BioPag> {
+  bool autorizacion= false;
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Registro Biometrico'),
+        centerTitle: true,
+      ),
+
+      floatingActionButton: autorizacion==true ? null : 
+      FloatingActionButton(
+        onPressed: ()async{
+          final autenticacion = await LAutorizacion.autenticar();
+          setState(() {
+            autorizacion= autenticacion;
+          });
+        }
+        ,child: Icon(Icons.fingerprint),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if(!autorizacion)
+            const Icon(
+              Icons.warning,
+              size: 60,
+            ),
+            if(autorizacion)
+            Icon(Icons.check_circle, size:60),
+            if(autorizacion)
+            ElevatedButton(
+              onPressed: (){
+                setState(() {
+                  autorizacion=false;
+                });
+              }, child: const Text('Remover Autorizacion')) 
+          ],
+        ),)
+
+    );
+  }
+}
