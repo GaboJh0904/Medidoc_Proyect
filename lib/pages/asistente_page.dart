8d458ee147bef027eb9f13b355b7a5345bd20ac0
GaboJh0904 <126207264+GaboJh0904@users.noreply.github.com>
2024-04-26 00:18:11 -0400

    Correcciones primera presentacion

--- lib/pages/asistente_page.dart
+++ lib/pages/asistente_page.dart
@@ -1,39 +1,55 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:medidoc_proyect/pages/menu_principal.dart';
 
-class AsistentePage extends StatelessWidget {
-  final String nombre ='Luan';
-
+class AsistentePage extends StatefulWidget {
   const AsistentePage({super.key});
 
   @override
+  _AsistentePageState createState() => _AsistentePageState();
+}
+
+class _AsistentePageState extends State<AsistentePage> {
+  final String nombre = 'Luan';
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 10),
+          () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MenuPrincipal()),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15.0,vertical: 10.0),
-              child: Text(
-                'Le damos la bienvenida Sr/a $nombre, me llamo Lia, soy su asistente personal',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.deepPurpleAccent,
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                ),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.0,vertical: 10.0),
+            child: Text(
+              'Le damos la bienvenida Sr/a $nombre, me llamo Lia, soy su asistente personal',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.deepPurpleAccent,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20.0),
-            Expanded(
-              child: Container(
-                alignment: Alignment.bottomRight,
-                child: Image.asset('assets/images/asistente1.png'),
-              ),
+          ),
+          SizedBox(height: 20.0),
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomRight,
+              child: Image.asset('assets/images/asistente1.png'),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/pages/menu_principal.dart
+++ lib/pages/menu_principal.dart
@@ -12,155 +12,195 @@ class MenuPrincipal extends StatelessWidget {
       ),
       drawer: NavbarOptions(),
       backgroundColor: Colors.white,
-      body: Center(
-        child: Navigator(
-          onGenerateRoute: ((settings) {
-            return MaterialPageRoute(
-              builder: (context) => _buildContent(context, settings),
-            );
-          }),
+      body: SafeArea(
+        child: Column(
+          children: [
+            HomeIndicator(), // Banderita "Inicio" alineada a la izquierda
+            Expanded(
+              child: MyCustomButtonGrid(), // Cuadrícula de botones ajustada
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: PlaceholderWidget(), // Espacio ajustado para imagen/video
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-Widget _buildContent(BuildContext context, RouteSettings settings) {
-  switch (settings.name) {
-    case '/':
-      return Column(
-        children: [
-          Expanded(
-            child: Center(
-                child: MyCustomButtonColumn()
-            ),
-          ),
-        ],
-      );
-    case '/asistente':
-      return AsistentePage();
-    case '/formulario':
-      return Formulario();
-    default:
-      return Container();
-  }
-}
-
-class MyCustomButtonColumn extends StatelessWidget {
+class HomeIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        MyCustomButton(
-          text: "Asistente Personal",
-          onPressed: () {
-            Navigator.pushNamed(
-                context, '/asistente');
-          },
+    // La mitad del ancho de la pantalla
+    double screenWidth = MediaQuery.of(context).size.width / 2;
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        width: screenWidth,
+        padding: EdgeInsets.all(8.0),
+        child: ClipPath(
+          clipper: BannerClipper(),
+          child: Container(
+            color: Colors.purple,
+            height: 30,
+            child: Center(
+              child: Text(
+                'Inicio',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
         ),
-        SizedBox(height: 22),
-        MyCustomButton(
-          text: "Formulario",
-          onPressed: () {
-            Navigator.pushNamed(
-                context, '/formulario');
-          },
-        ),
-        SizedBox(height: 22),
-        MyCustomButton(
-          text: "Opcion Tres",
-          onPressed: () {},
-        ),
-        SizedBox(height: 22),
-        MyCustomButton(
-          text: "Opcion Cuatro",
-          onPressed: () {},
-        ),
-      ],
+      ),
     );
   }
 }
 
-class MyCustomButton extends StatefulWidget {
+class BannerClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width * 0.75, size.height);
+    path.lineTo(size.width, size.height / 2);
+    path.lineTo(size.width * 0.75, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class MyCustomButtonGrid extends StatelessWidget {
+  final List<String> options = [
+    "Historial médico", "Botón de alerta", "Solicitud de cita", // Añade los títulos
+    "Cancelación de cita", "Mapa de hospitales", "Cuestionarios médicos",
+    "Teleconsulta", "Ver cita programada", "Receta médica",
+    "BPO", "Tutoriales de primeros auxilios", "Emergencia SOS",
+    "Consulta en casa", "Chat asistente", "Calendario", // Asegúrate de tener 15 títulos
+  ];
+
+  void Function() _getActionForItem(String item) {
+    // Aquí puedes definir lo que cada opción debe hacer
+    return () {
+      print('Se seleccionó: $item');
+      // Aquí iría la lógica para cada acción
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 1,
+      ),
+      itemCount: options.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: _getActionForItem(options[index]),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Center(
+              child: Text(
+                options[index],
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyCustomButton extends StatelessWidget {
   final String text;
-  final VoidCallback? onPressed;
-  final double width;
-  final double height;
+  final VoidCallback onPressed;
 
   MyCustomButton({
     required this.text,
     required this.onPressed,
-    this.width = 250.0,
-    this.height = 80.0,
   });
 
   @override
-  _MyCustomButtonState createState() => _MyCustomButtonState();
+  Widget build(BuildContext context) {
+    // Botón simplificado sin estado interno
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0), // Espaciado entre botones
+        width: 80, // Ancho del botón
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(16.0), // Padding dentro del botón
+              decoration: BoxDecoration(
+                color: Colors.purple, // Color de fondo del botón
+                borderRadius: BorderRadius.circular(10), // Borde redondeado
+              ),
+              child: Icon(
+                Icons.add, // Icono del botón, cámbialo por el que necesites
+                color: Colors.white,
+                size: 24, // Tamaño del icono
+              ),
+            ),
+            SizedBox(height: 8), // Espaciado entre icono y texto
+            Text(
+              text, // Texto del botón
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 10,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyCustomButtonState extends State<MyCustomButton> {
-  bool isPressed = false;
 
+class PlaceholderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (_) {
-        setState(() {
-          isPressed = true;
-        });
-      },
-      onTapUp: (_) {
-        setState(() {
-          isPressed = false;
-        });
-        // Aquí llamarías a la función que deseas que se ejecute al presionar.
-        widget.onPressed?.call();
-      },
-      onTapCancel: () {
-        setState(() {
-          isPressed = false;
-        });
-      },
-      child: SizedBox(
-        width: widget.width, // Usa el ancho pasado al widget
-        height: widget.height,
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 200), // Duración de la animación
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            gradient: LinearGradient(
-              colors: isPressed
-                  ? [
-                      Colors.purple,
-                      Colors.purpleAccent
-                    ] // Colores cuando está presionado
-                  : [
-                      Color(0xFF6F35A5),
-                      Color(0xFFB388FF)
-                    ], // Colores originales
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                spreadRadius:
-                    isPressed ? 0 : 2, // Eliminar sombra cuando está presionado
-                blurRadius: isPressed ? 0 : 6,
-                offset: isPressed ? Offset(0, 0) : Offset(0, 2),
-              ),
-            ],
-          ),
-          child: Text(
-            widget.text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 13,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
+    // Reducir tamaño y mover a la esquina inferior derecha
+    return Padding(
+      padding: EdgeInsets.only(
+        right: 16.0, // Ajustar según sea necesario
+        bottom: 16.0, // Ajustar según sea necesario
+      ),
+      child: Container(
+        width: 50, // Ancho del espacio para imagen/video
+        height: 50, // Altura del espacio para imagen/video
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        alignment: Alignment.center,
+        child: Icon(
+          Icons.person_outline,
+          size: 50,
+          color: Colors.grey,
         ),
       ),
     );
